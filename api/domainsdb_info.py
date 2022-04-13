32850a86e69696f4928fa78e1f58c55111c0cfae
Cthupby <Cthupby@gmail.com>
2022-04-13 17:44:54 +0300

    Update domainsdb info

--- api/domainsdb_info.py
+++ api/domainsdb_info.py
@@ -2,33 +2,37 @@ import aiohttp
 import asyncio
 import time
 
-
-start_time = time.time()
 domain_info = []
-urls_list = ['losst.ru']
+start_time = time.time()
+urls = ['http://www.losst.ru', 'http://www.losst.eu']
+# url = urls.replace("http://www.","")
 
 
-async def get_domain_info(session, find_url: str) -> str:
-    find_url = f'https://api.domainsdb.info/v1/domains/search?domain={find_url}'
+async def get_domain_info(session, url: str) -> str:
+    find_url = f'https://api.domainsdb.info/v1/domains/search?domain={url}'
     async with session.get(find_url) as resp:
-        assert resp.status == 200
+#        assert resp.status == 200
         resp_json = await resp.json()
-        for resp in resp_json['domains']:
-            for c, data in resp.items():
-                domain_info.append(data)
+        try:
+            for resp in resp_json['domains']:
+                for data in resp.values():
+                    domain_info.append(data)
+        except:
+            domain_info.append(resp_json)
         return resp_json
 
 
-async def load_domain_data(urls_list):
+async def load_domain_data(url):
     async with aiohttp.ClientSession() as session:
         tasks = []
-        for url in urls_list:
-            task = asyncio.create_task(get_domain_info(session, url))
-            tasks.append(task)
+        task = asyncio.create_task(get_domain_info(session, url))
+        tasks.append(task)
         await asyncio.gather(*tasks)
 
 
-asyncio.run(load_domain_data(urls_list))
+for url in urls:
+    asyncio.run(load_domain_data(url.replace("http://www.","")))
+
 
 end_time = time.time() - start_time
 print(domain_info)
