7eb6b87a085581795fca24cedfc524a093356370
Cthupby <Cthupby@gmail.com>
2022-09-10 15:45:20 +0300

    update tests

---
+++ tests/test_domain_api.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from api.domain_api import get_domain_info, load_domain_data
+
+
+class DomainApiTestCase(TestCase):
+    def get_domain_info(self):
+        pass
+
+    def load_domain_data(self):
+        pass
---
+++ tests/test_parser.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+from api.parser import get_links
+
+
+class ParserTestCase(TestCase):
+    def get_links(self):
+        pass
